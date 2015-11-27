f3912fde7176e7b85315d26a521f974cc7d4bea6
Panu Tulimäki <panu.tulimaki@anders.fi>
2015-11-27 14:20:16 +0200

    Update version to 1.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 if __name__ == '__main__':
     setuptools.setup(
         name="shoop-carousel",
-        version="0.5.0",
+        version="1.0.0",
         description="Shoop Carousel",
         packages=setuptools.find_packages(),
         include_package_data=True,
