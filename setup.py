e0ae5087bdc2a1f08e42a70e25496b749828fa81
Tuomas Suutari <tuomas.suutari@shoop.io>
2016-05-04 13:20:47 +0300

    setup.py: Do build tasks
    Will compile the PO files to MO files at "setup.py build" (when
    translations are added).
    
    Refs SHOOP-2341/SHOOP-2510

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 import setuptools
 
+try:
+    import shoop_setup_utils
+except ImportError:
+    shoop_setup_utils = None
+
 
 if __name__ == '__main__':
     setuptools.setup(
@@ -8,5 +13,9 @@ if __name__ == '__main__':
         description="Shoop Carousel",
         packages=setuptools.find_packages(),
         include_package_data=True,
-        entry_points={"shoop.addon": "shoop_carousel=shoop_carousel"}
+        entry_points={"shoop.addon": "shoop_carousel=shoop_carousel"},
+        cmdclass=(shoop_setup_utils.COMMANDS if shoop_setup_utils else {}),
+        install_requires=[
+            'shoop>=3.0,<5',
+        ],
     )
