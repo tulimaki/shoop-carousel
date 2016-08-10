7cfca302799e394e34e373208f6adff393963049
Panu Tulimäki <panu@shuup.com>
2016-08-10 11:10:43 -0700

    Bump version to 1.5.7

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 if __name__ == '__main__':
     setuptools.setup(
         name="shuup-carousel",
-        version="1.5.6",
+        version="1.5.7",
         description="Shuup Carousel",
         packages=setuptools.find_packages(),
         include_package_data=True,
